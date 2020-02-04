ca41a0a87e38e57456bd2038097e6d85ffab2f63
Zkarts <enmolenaar@gmail.com>
2020-02-04 22:11:14 +0100

    Initial commit, made setup for 2d sprites in a 3d environment, added character movement

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private const string horizontalAxisName = "Horizontal";
+    private const string verticalAxisName = "Vertical";
+
+    [SerializeField]
+    private float movementScale = 0.5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(movementScale * Input.GetAxis(horizontalAxisName), 0, movementScale * Input.GetAxis(verticalAxisName));
+    }
+}
