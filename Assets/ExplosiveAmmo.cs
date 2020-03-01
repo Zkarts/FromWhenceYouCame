1ab75a14ce41d215e29d4eb8d51f602472899e8f
Zkarts <enmolenaar@gmail.com>
2020-03-01 17:07:18 +0100

    Added Explosive Ammo, played around with joints for a chain (it's still very jittery and VERY hard to get stable...)

---
+++ Assets/ExplosiveAmmo.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosiveAmmo : MonoBehaviour
+{
+
+    [SerializeField]
+    private float explosionRadius = 5.0f;
+    [SerializeField]
+    private float power = 5.0f;
+
+    private new Rigidbody rigidbody;
+    private int destructibleLayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        destructibleLayer = LayerMask.NameToLayer("Destructible");
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == destructibleLayer)
+        {
+            Debug.Log("BOOM!!!");
+            Vector3 explosionPos = transform.position;
+            Collider[] colliders = Physics.OverlapSphere(explosionPos, explosionRadius);
+            //Add explosive force to all nearby rigidbodies
+            foreach (Collider hit in colliders)
+            {
+                Rigidbody rb = hit.GetComponent<Rigidbody>();
+
+                if (rb != null && hit.gameObject.layer == destructibleLayer)
+                {
+                    rb.AddExplosionForce(power, explosionPos, explosionRadius, 1.0F, ForceMode.Impulse);
+                }
+            }
+        }
+
+        //Destroy itself
+        Destroy(gameObject);
+    }
+
+}
---
+++ Assets/PhysicsMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsMovement : MonoBehaviour
+{
+    private new Rigidbody rigidbody;
+    [SerializeField]
+    private Vector3 moveForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveForce = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        rigidbody.AddForce(moveForce, ForceMode.VelocityChange);
+    }
+}
